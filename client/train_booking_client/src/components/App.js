7f32cf33c624ef63bed6b4c6adfcab7298d76afc
Sachith Rukshan <sachithrukshanmail@gmail.com>
2019-05-19 12:51:42 +0530

    commit before branch

---
+++ client/train_booking_client/src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Head from './Head';
+import Body from './Body';
+import Footer from './Footer';
+
+function App() {
+  return (
+    <div id="site-content">
+      <Head />
+      <Body />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
